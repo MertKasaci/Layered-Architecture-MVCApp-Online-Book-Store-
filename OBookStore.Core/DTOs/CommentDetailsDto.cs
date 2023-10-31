1a68a75eeef4c0cb67b8f502fc8079f206c65184
MertKASACİ <ksccmrtt@gmail.com>
2023-10-31 20:04:52 +0300

    Comment Details DTO

--- OBookStore.Core/DTOs/CommentDetailsDto.cs
+++ OBookStore.Core/DTOs/CommentDetailsDto.cs
@@ -10,8 +10,8 @@ namespace OBookStore.Core.DTOs
     {
         public string Title { get; set; }
         public string Body { get; set; }
-        public int NumberOfLikes { get; set; }
-        public int NumberOfDislikes { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
         public int UserId { get; set; }
         public int BookId { get; set; }
 
